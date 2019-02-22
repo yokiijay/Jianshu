1d73fc803727a6ab8d4cc4bcb40ae283baf3e346
Xmac <xmaczone@Xmac.local>
2019-02-22 22:56:16 +0800

    changed

--- src/App.js
+++ src/App.js
@@ -1,34 +1,16 @@
 import React, { Component, Fragment } from 'react'
-import { connect } from 'react-redux'
-import GlobalStyle from './globalStyle'
-import { BrowserRouter as Router, Link, Route, Redirect } from 'react-router-dom'
 
 //Components
-import Header from './Common/Header'
-
-//Pages
-import Home from './Pages/home'
-import Detail from './Pages/detail'
-
+import Header from './Components/Header'
 
 class App extends Component {
   render(){
     return(
       <Fragment>
-        <GlobalStyle />
         <Header />
-
-        <Router>
-          <Fragment>
-            <Route exact path='/' component={ Home } />
-            <Route exact path='/home' render={()=><Redirect to='/' />} />
-            <Route exact path='/detail' component={ Detail } />
-          </Fragment>
-        </Router>
-
       </Fragment>
     )
   }
 }
 
-export default connect()(App)
+export default App
---
+++ src/Components/Header/index.js
@@ -0,0 +1,16 @@
+import React, { Component, Fragment } from 'react'
+
+//Components
+import { Flex, Box } from '@rebass/grid'
+
+class Header extends Component {
+	render(){
+		return(
+			<Fragment>
+				
+			</Fragment>
+		)
+	}
+}
+
+export default Header
